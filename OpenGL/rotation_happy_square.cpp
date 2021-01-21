bc68f144e001a594cdd9ee35e6bbb3eb02b63f90
ywon <joy323129@gmail.com>
2021-01-21 19:05:58 +0900

    2021_01_21

--- OpenGL/rotation_happy_square.cpp
+++ OpenGL/rotation_happy_square.cpp
@@ -168,7 +168,7 @@ int main()
 
         // create transformations
         glm::mat4 transform = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
-        transform = glm::translate(transform, glm::vec3(0.5f, -0.5f, 0.0f));
+        transform = glm::translate(transform, glm::vec3(0.5f, 0.5f, 0.0f));
         transform = glm::rotate(transform, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
 
         // get matrix's uniform location and set matrix
